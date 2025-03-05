2c79d21c66d2f91207651f19c953d88d2cf528a5
OlegD <o.dolgarev@gmail.com>
2025-03-05 23:03:56 +0200

    refactor: rewrite the code to modern ES6

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 /* global Package */
 
 Package.describe({
-  name: 'liberation:subscription-audit',
-  summary: 'Audit of subscriptions used by meteor application.',
-  version: '0.0.4',
-  git: 'https://github.com/dolgarev/meteor-subscription-audit.git'
-})
+  name: "liberation:subscription-audit",
+  summary: "Analyzing subscriptions used by Meteor applications.",
+  version: "0.0.4",
+  git: "https://github.com/dolgarev/meteor-subscription-audit.git",
+});
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.5')
-  api.use(['ecmascript', 'ejson', 'underscore'], 'client')
-  api.mainModule('main.js', 'client')
-  api.export('SA')
-})
+  api.versionsFrom("1.5"); // Specify the minimum Meteor version
+  api.use(["ecmascript", "ejson"], "client"); // Dependencies for the client
+  api.mainModule("main.js", "client"); // Main entry point for the client
+  api.export("SA"); // Export the SA symbol
+});
